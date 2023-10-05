e246e7cb6c44fcb7d06dde410c55ff87fb7d3948
stul <131748628+Jitry3@users.noreply.github.com>
2023-10-05 09:42:15 +0800

    Create DashboardViewModel.kt

---
+++ app/src/main/java/com/android/kvc/xiao/v2/miui/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,10 @@
+package com.android.kvc.xiao.v2.miui.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    
+}

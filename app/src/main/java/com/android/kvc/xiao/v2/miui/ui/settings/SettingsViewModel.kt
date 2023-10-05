ea0fe095e7e68b935a88db2653a5e96c9481e815
stul <131748628+Jitry3@users.noreply.github.com>
2023-10-05 09:43:49 +0800

    Create SettingsViewModel.kt

---
+++ app/src/main/java/com/android/kvc/xiao/v2/miui/ui/settings/SettingsViewModel.kt
@@ -0,0 +1,10 @@
+package com.android.kvc.xiao.v2.miui.ui.settings
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+
+    
+}

eaeeb805f460514fca94c5d2fa1e72aec9ff28e9
stul <131748628+Jitry3@users.noreply.github.com>
2023-10-12 20:37:48 +0800

    Create Apps1.java

---
+++ app/src/main/java/com/android/kvc/xiao/v2/miui/activity/v1/Apps1.java
@@ -0,0 +1,11 @@
+package com.android.kvc.xiao.v2.miui.activity.v1;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Apps1 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

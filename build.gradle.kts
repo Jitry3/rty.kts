1653080fc223e0da4d873ac2af37b0a26e0c6621
stul <131748628+Jitry3@users.noreply.github.com>
2023-10-21 09:56:25 +0800

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     id("com.android.library") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false    
     id("org.jetbrains.kotlin.jvm") version "1.9.10" apply false 
-    id("com.google.gms.google-services") version "4.4.0" apply false
+    
 }
 
 tasks.register<Delete>("clean") {

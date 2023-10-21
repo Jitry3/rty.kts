ed4100059d606576c765ece1ce50fc384c1ff9a7
stul <131748628+Jitry3@users.noreply.github.com>
2023-10-21 09:59:54 +0800

    Add files via upload

---
+++ app/src/main/java/com/android/kvc/xiao/v2/miui/activity/Apps5.java
@@ -0,0 +1,11 @@
+package com.android.kvc.xiao.v2.miui.activity;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Apps5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
